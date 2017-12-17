c6e1ec969b6318fcf1eb09667227d0b4669992a0
joelmandell <joelmandell@gmail.com>
2017-12-17 19:29:37 +0100

    Added history on the pupils-overview.

--- src/components/Pupils.vue
+++ src/components/Pupils.vue
@@ -1,13 +1,44 @@
 <template>
     <div>
         <router-view></router-view>
+        <h3>Pupil overview</h3>
+
+        <select @change="setPupil($event)" v-model="pupilId">
+            <option :value="p.id" :key="p.id" v-for="(p) in pupils">
+                {{ p.firstName}} {{ p.lastName}}
+            </option>
+        </select>
+        <button class="primary button" @click="change()">Edit pupil</button>
         <button class="primary button"><router-link style="color:#fff" to="/pupils/add">Add new</router-link></button>
-        <p v-for="p in pupils" >
-            <router-link v-show="typeof p.id!='undefined'" :to='"/pupils/edit/"+p.id'>{{ p.firstName }} {{ p.lastName }}</router-link>
-        </p>        
+        <h3>Tasks</h3>
+        {{ pupils.length > 0}}
+        <table v-if="pupils.length>0">
+            <thead>
+                <th>Year</th>
+                <th>Week</th>
+                <th>Assignment</th>
+                <th>Lesson</th>
+                <th>Participant</th>
+            </thead>
+            <tbody>
+                <tr :key="'tasks'+idx" v-for="(task,idx) in pupils[currentPupilIdx].tasks">
+                    <td>{{task.year}}</td>
+                    <td>{{task.week}}</td>
+                    <td> 
+                        <span v-if="task.taxanomies != null">
+                            {{ task.taxanomies.name }}
+                        </span>
+                        <span v-else>
+                            -
+                        </span>
+                    </td>
+                    <td>{{task.lessonId}}</td>
+                    <td>{{task.participant ? "YES" : "NO"}}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
-
 
 <script>
 import gql from 'graphql-tag'
@@ -16,7 +47,24 @@ export default {
     data() {
         return {
             pupils:[],
+            pupilId:1,
+            currentPupilIdx:0,
+            client:null
         }
+    },
+    methods: {
+        setPupil: function(evt) 
+        {
+            this.currentPupilIdx = evt.target.selectedOptions[0].index
+            this.pupilId = evt.target.selectedOptions[0].value
+        },
+        change: function()
+        {
+            this.$router.push("/pupils/edit/"+this.pupilId)
+        }
+    },
+    created: function() {
+        this.client = this.$apollo.provider.defaultClient
     },
     apollo: {
         pupils: {
@@ -29,6 +77,19 @@ export default {
                     conducting
                     comments
                     woman
+                    tasks {
+                        id
+		                year
+                        week
+                        taxanomies {
+                            name
+                            participant
+                        }
+		                taxanomyId
+                        pupilId
+		                lessonId
+		                participant
+                    }
                 }  
             }`        
         }
